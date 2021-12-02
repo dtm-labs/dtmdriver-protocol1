cf072917c85bbc0848d93385c5d15fe8bda38d31
yedf2 <120050102@qq.com>
2021-12-02 17:52:41 +0800

    sample updated

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/yedf/dtmcli v1.5.1
-	github.com/yedf/dtmdriver v0.0.0-20211201142343-983b402db011
+	github.com/yedf/dtmdriver v0.0.0-20211202083012-e340578709e1
 	github.com/yedf/dtmgrpc v1.5.1 // indirect
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
--- sampledriver/sampledriver.go
+++ sampledriver/sampledriver.go
@@ -1,6 +1,10 @@
 package sample
 
 import (
+	"fmt"
+	"net/url"
+	"strings"
+
 	"github.com/yedf/dtmdriver"
 	"google.golang.org/grpc/resolver"
 )
@@ -14,7 +18,7 @@ func (d *sampleDriver) GetName() string {
 	return "dtm-sample-driver"
 }
 
-func (d *sampleDriver) RegisterService(url string, endpoint string) error {
+func (d *sampleDriver) RegisterGrpcService(url string, endpoint string) error {
 	// 如果使用etcd，polaris等注册/发现组件的话，那么在这里，将endpoint注册到相应的url中
 	// 这里的sample仅作为演示用，没有实际注册
 	return nil
@@ -22,6 +26,22 @@ func (d *sampleDriver) RegisterService(url string, endpoint string) error {
 
 func (d *sampleDriver) RegisterGrpcResolver() {
 	resolver.Register(&sampleBuilder{})
+}
+
+func (d *sampleDriver) ParseServerMethod(uri string) (server string, method string, err error) {
+	if !strings.Contains(uri, "//") { // 处理无scheme的情况，如果您没有直连，可以不处理
+		sep := strings.IndexByte(uri, '/')
+		if sep == -1 {
+			return "", "", fmt.Errorf("bad url: '%s'. no '/' found", uri)
+		}
+		return uri[:sep], uri[sep:], nil
+
+	}
+	u, err := url.Parse(uri)
+	if err != nil {
+		return "", "", err
+	}
+	return u.Scheme + "://" + u.Host, u.Path, nil
 }
 
 func init() {
